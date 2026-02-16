c00bb14f7fb62216b64833205ce8e40effbcbd3b
Yaroslav Malyanov <y.a.malyanoff@gmail.com>
2026-02-16 11:21:56 +0300

    Update getp.py

--- getp.py
+++ getp.py
@@ -1,55 +1,40 @@
 import RPi.GPIO as GPIO
 
-class R2R_DAC:
-    def __init__(self, gpio_bits, dynamic_range, verbose=False):
-        self.gpio_bits = gpio_bits
+
+class PWM_DAC:
+    def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose=False):
+        self.gpio_pin = gpio_pin
+        self.pwm_frequency = pwm_frequency
         self.dynamic_range = dynamic_range
         self.verbose = verbose
 
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.gpio_bits, GPIO.OUT, initial=0)
+        GPIO.setup(self.gpio_pin, GPIO.OUT)
 
-    def deinit(self):
-        GPIO.output(self.gpio_bits, 0)
-        GPIO.cleanup()
+        self.pwm = GPIO.PWM(self.gpio_pin, self.pwm_frequency)
 
-    def dec2bin(self, value):
-        return [int(bit) for bit in bin(value)[2:].zfill(8)]
-
-    def set_number(self, number):
-        if number < 0:
-            number = 0
-        if number > 255:
-            number = 255
-
-        binary = self.dec2bin(number)
-        GPIO.output(self.gpio_bits, binary)
+        self.pwm.start(0)
 
         if self.verbose:
-            print(f"Число: {number}, Биты: {binary}")
+            print("PWM DAC инициализирован")
+
+    def deinit(self):
+        self.pwm.ChangeDutyCycle(0)
+        self.pwm.stop()
+        GPIO.cleanup()
+
+        if self.verbose:
+            print("PWM DAC остановлен, GPIO очищены")
 
     def set_voltage(self, voltage):
         if not (0.0 <= voltage <= self.dynamic_range):
             if self.verbose:
-                print(f"Напряжение {voltage:.2f} В вне диапазона (0 - {self.dynamic_range:.3f} В). Устанавливаем 0 В.")
-            number = 0
+                print(f"Напряжение {voltage:.3f} В вне диапазона (0 - {self.dynamic_range:.3f} В). Устанавливаем 0 В.")
+            duty_cycle = 0
         else:
-            number = round(voltage / self.dynamic_range * 255)
+            duty_cycle = voltage / self.dynamic_range * 100
 
-        self.set_number(number)
+        self.pwm.ChangeDutyCycle(duty_cycle)
 
-
-if __name__ == "__main__":
-    try:
-        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
-
-        while True:
-            try:
-                voltage = float(input("Введите напряжение в Вольтах: "))
-                dac.set_voltage(voltage)
-
-            except ValueError:
-                print("Вы ввели не число. Попробуйте ещё раз\n")
-
-    finally:
-        dac.deinit()
+        if self.verbose:
+            print(f"Напряжение: {voltage:.3f} В → Duty cycle: {duty_cycle:.2f}%")
