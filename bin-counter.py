aa114c9639e22bceba86967b262f43e2bc6aa504
Yaroslav Malyanov <y.a.malyanoff@gmail.com>
2026-03-02 09:28:04 +0300

    Update bin-counter.py

--- bin-counter.py
+++ bin-counter.py
@@ -1,29 +1,34 @@
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BCM)
-leds = [16, 12, 25, 17, 27, 23, 22, 24]
+leds = [16,12,25,17,27,23,22,24]
 GPIO.setup(leds, GPIO.OUT)
 
-GPIO.output(leds,0)
-up = 20
-down = 21
-GPIO.setup(up, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(down, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.output(leds, 0)
+
+up = 9
+down = 10
+
+GPIO.setup(up, GPIO.IN)
+GPIO.setup(down, GPIO.IN)
 
 num = 0
-def dec2bin(val):
-    return [int(bit) for bit in bin(val)[2:].zfill(8)]
+
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+
 sleep_time = 0.2
-Max_NUM = 255
 while True:
-    if GPIO.input(up):
-        num += 1
+    if  GPIO.input(up):
+        num = num + 1
         print(num, dec2bin(num))
-        GPIO.output(leds, dec2bin(num))
         time.sleep(sleep_time)
-    if GPIO.input(down):
-        num -= 1
+
+    if  GPIO.input(down):
+        num = num - 1
         print(num, dec2bin(num))
-        GPIO.output(leds, dec2bin(num))
         time.sleep(sleep_time)
+
+    GPIO.output(leds, dec2bin(num))
         
