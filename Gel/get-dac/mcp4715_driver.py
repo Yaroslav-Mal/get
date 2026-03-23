c5c9bf8cb96f1c6f82f3b8463af5bd8ae836b4cc
Yaroslav Malyanov <y.a.malyanoff@gmail.com>
2026-03-23 09:58:11 +0300

    Update mcp4715_driver.py

--- Gel/get-dac/mcp4715_driver.py
+++ Gel/get-dac/mcp4715_driver.py
@@ -1,6 +1,8 @@
 import smbus
+
+
 class MCP4725:
-    def __init__ (self,dynamic_range, address = 0x61, verbose = True):
+    def __init__(self, dynamic_range, address=0x61, verbose=True):
         self.bus = smbus.SMBus(1)
 
         self.address = address
@@ -9,34 +11,54 @@ class MCP4725:
 
         self.verbose = verbose
         self.dynamic_range = dynamic_range
-    
-    
+
     def deinit(self):
         self.bus.close()
 
-    def set_number(self,number):
+    def set_number(self, number):
         if not isinstance(number, int):
-            print("На вход можно подавать только целые числа")
+            print("На вход ЦАП можно подавать только целые числа")
+            return
 
         if not (0 <= number <= 4095):
-            print("Число выходит за разрядность MCP4752 (12 бит)")
+            print("Число выходит за разрядность MCP4725 (12 бит)")
+            return
 
-        first_byte = self.wm | self.pds | number >> 8
+        first_byte = self.wm | self.pds | (number >> 8)
         second_byte = number & 0xFF
-        self.bus.write_byte_data(0x61,first_byte, second_byte)
+
+        self.bus.write_byte_data(self.address, first_byte, second_byte)
 
         if self.verbose:
-            print(f"Число: {number} , отправленные I2c данные: [0x{(self.address << 1):02X}, 0x{first_byte:02X} , 0x{second_byte:02X}]\n")
-    def set_voltage(self,voltage):
-        self.set_number(round(voltage/self.dynamic_range*4095))
+            print(
+                f"Число: {number}, отправленные по I2C данные: "
+                f"[0x{(self.address << 1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}]"
+            )
+
+    def set_voltage(self, voltage):
+        if not (0.0 <= voltage <= self.dynamic_range):
+            print(
+                f"Напряжение выходит за динамический диапазон "
+                f"(0.0 ... {self.dynamic_range:.2f} В)"
+            )
+            return
+
+        number = round(voltage / self.dynamic_range * 4095)
+        self.set_number(number)
+
+
 if __name__ == "__main__":
+    dac = None
     try:
-        MCP = MCP4725(5 , True)
+        dac = MCP4725(5.0, 0x61, True)
+
         while True:
             try:
-                voltage = float (input("Введите напряжение в Вольтах: "))
-                MCP.set_voltage(voltage)
+                voltage = float(input("Введите напряжение в Вольтах: "))
+                dac.set_voltage(voltage)
             except ValueError:
-                print("Вы не ввели число.Попробуйте езе раз")
+                print("Вы ввели не число. Попробуйте еще раз\n")
+
     finally:
-        MCP.deinit()
+        if dac is not None:
+            dac.deinit()
